6d71b861155bbbbe9c12015a26ad560b71cfeec8
Jason Skipper <jasonskipper@gmail.com>
2016-02-08 08:57:50 -0500

    Changes

--- web/app/themes/starter/footer.php
+++ web/app/themes/starter/footer.php
@@ -63,5 +63,22 @@
     <?php endif; ?>
   <?php endif; ?>
 <?php wp_footer(); ?>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.hohnet.com"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//skipperinnovations.com/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 2]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//skipperinnovations.com/piwik/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 </body>
 </html>
